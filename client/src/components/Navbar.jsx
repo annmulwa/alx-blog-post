14d6337b0a3cd4fb7b4599a55a58d1ef046bd5f8
annmulwa <annmulwa2000@gmail.com>
2024-09-18 22:25:10 +0300

    Fetching recommended posts

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,9 @@ const Navbar = () => {
         <div className='navbar'>
             <div className="container">
                 <div className="logo">
+                    <Link to="/">
                     <img src={ Logo } alt="" />
+                    </Link>
                 </div>
                 <div className="links">
                     <Link className='link' to="/?cat=art">
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,34 +1,67 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import {Link, useLocation, useNavigate} from 'react-router-dom';
 import Edit from '../img/edit.png';
 import Delete from '../img/delete.png';
 import Menu from '../components/Menu';
+import axios from 'axios';
+import moment from 'moment';
+import { AuthContext } from '../context/authContext';
 
 const Single = () => {
+
+    const [post, setPost] = useState({});
+
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const postId = location.pathname.split("/")[2]
+
+    const {currentUser} = useContext(AuthContext);
+
+    useEffect(()=>{
+        const fetchData = async ()=>{
+            try{
+                const res = await axios.get(`/posts/${postId}`);
+                setPost(res.data);
+            }catch(err){
+                console.log(err);
+            }
+        };
+        fetchData();
+    },[postId]);
+
+    const handleDelete = async ()=>{
+        try{
+            await axios.delete(`/posts/${postId}`);
+            navigate("/");
+        }catch(err){
+            console.log(err);
+        }
+    }
     return (
         <div className="single">
             <div className="content">
-                <img src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                <img
+                    src={post?.img}
+                    alt=""
+                />
             <div className="user">
-                <img src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                {post.userImg && <img src={post.userImg} alt="" />}
             <div className="info">
-                <span>John</span>
-                <p>Posted 2 days ago</p>
+                <span>{post.username}</span>
+                <p>Posted {moment(post.date).fromNow()}</p>
             </div>
+            {currentUser.username === post.username && (
             <div className="edit">
                 <Link to={`/write?edit=2`}>
                 <img src={Edit} alt="" />
                 </Link>
-                <img src={Delete} alt="" />
+                <img onClick={handleDelete} src={Delete} alt="" />
             </div>
+            )}
             </div>
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-            <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
-            <br />
-            <br/>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
-            </p>
+            <h1>{post.title}</h1>
+            {post.desc}
             </div>
             <Menu />
         </div>
